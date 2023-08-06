78b5c26238cc46e09e16d8aac36793e8f5450358
wimdavies <william.johnston.davies@gmail.com>
2023-08-06 17:29:28 +0100

    Add tests for happy path of Normal Items

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -9,12 +9,26 @@ RSpec.describe GildedRose do
     end
 
     context 'Normal Item' do
-      it 'decrements sell_in and quality on every tick' do
+      it 'decrements both sell_in and quality on every tick' do
         items = [Item.new("Dagger", 47, 47)]
         gilded_rose = GildedRose.new(items)
         4.times { gilded_rose.update_quality } 
         expect(items[0].sell_in).to eq 43
         expect(items[0].quality).to eq 43
+      end
+
+      it 'decreases quality at 2x rate when sell_in is less than 0' do
+        items = [Item.new("Dagger", 0, 10)]
+        gilded_rose = GildedRose.new(items)
+        4.times { gilded_rose.update_quality } 
+        expect(items[0].quality).to eq 2
+      end
+
+      it 'never decreases quality below 0' do
+        items = [Item.new("Dagger", 47, 43)]
+        gilded_rose = GildedRose.new(items)
+        500.times { gilded_rose.update_quality } 
+        expect(items[0].quality).to eq 0
       end
     end
   end
