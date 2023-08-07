59105588cb4ada009b24b247cc814f70ba23a099
wimdavies <william.johnston.davies@gmail.com>
2023-08-07 15:26:48 +0100

    Remove original conditional statements (and associated extracted methods)

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -16,42 +16,6 @@ class GildedRose
       when 'Sulfuras, Hand of Ragnaros'
         return
       end
-
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        decrement_quality_if_greater_than_zero(item)
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-
-      decrement_sell_in(item)
-
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            decrement_quality_if_greater_than_zero(item)
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
     end
   end
 
@@ -79,16 +43,6 @@ class GildedRose
     item.quality += 1
     item.quality += 1 if item.sell_in < 10
     item.quality += 1 if item.sell_in < 5
-  end
-
-  def decrement_sell_in(item)
-    item.sell_in = item.sell_in - 1
-  end
-
-  def decrement_quality_if_greater_than_zero(item)
-    if item.quality > 0
-      item.quality = item.quality - 1
-    end
   end
 end
 
