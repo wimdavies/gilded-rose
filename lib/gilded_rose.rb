6bfb0a731c41c65ff326e75e231ff5c2ff9398cd
wimdavies <william.johnston.davies@gmail.com>
2023-08-09 16:51:38 +0100

    Refactor conditionals into Updater factory with polymorphic subclasses

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -5,36 +5,57 @@ class GildedRose
 
   def update_quality
     @items.each do |item|
-      case item.name
-      when 'Aged Brie'
-        aged_brie_update(item)
-      when 'Backstage passes to a TAFKAL80ETC concert'
-        backstage_pass_update(item)
-      when 'Sulfuras, Hand of Ragnaros'
-        sulfuras_update(item)
-      else
-        normal_update(item)
-      end
+      Updater.for(item).update
     end
   end
+end
 
-  def normal_update(item)
+class Updater
+  def self.for(item)
+    case item.name
+    when 'Aged Brie'
+      BrieUpdater
+    when /backstage pass/i
+      PassUpdater
+    when 'Sulfuras, Hand of Ragnaros'
+      SulfurasUpdater
+    else
+      NormalUpdater
+    end.new(item)
+  end
+
+  attr_accessor :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def update
+  end
+end
+
+class NormalUpdater < Updater
+  def update
     item.sell_in -= 1
     return if item.quality <= 0
 
     item.quality -= 1
     item.quality -= 1 if item.sell_in <= 0 && item.quality > 1
   end
+end
 
-  def aged_brie_update(item)
+class BrieUpdater < Updater
+  def update
     item.sell_in -= 1
     return if item.quality >= 50
 
     item.quality += 1
     item.quality += 1 if item.sell_in <= 0 && item.quality < 50
   end
+end
 
-  def backstage_pass_update(item)
+class PassUpdater < Updater
+  def update
     item.sell_in -= 1
     return item.quality = 0 if item.sell_in < 0
     return if item.quality >= 50
@@ -43,13 +64,10 @@ class GildedRose
     item.quality += 1 if item.sell_in < 10 && item.quality < 50
     item.quality += 1 if item.sell_in < 5 && item.quality < 50
   end
-
-  def sulfuras_update(item)
-    return
-  end
 end
 
-
+class SulfurasUpdater < Updater
+end
 
 class Item
   attr_accessor :name, :sell_in, :quality
