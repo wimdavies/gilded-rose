a5fa993146a3228ec896ef71c7802f2fb0a96308
Kay Lack <kay@makers.tech>
2023-08-23 14:57:46 +0100

    Alternative design

--- Gemfile
+++ Gemfile
@@ -7,3 +7,5 @@ source "https://rubygems.org"
 gem "rspec", "~> 3.12"
 
 gem "simplecov", "~> 0.22.0"
+
+gem "simplecov-console", "~> 0.9.1"
--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,20 +1,35 @@
 # frozen_string_literal: true
 
 require_relative 'item'
-require_relative 'updater'
+require_relative 'normal_updater'
 require_relative 'brie_updater'
 require_relative 'pass_updater'
 require_relative 'sulfuras_updater'
 require_relative 'conjured_updater'
 
+DEFAULT_UPDATERS = [
+  ConjuredUpdater.new,
+  BrieUpdater.new,
+  PassUpdater.new,
+  SulfurasUpdater.new,
+  NormalUpdater.new
+].freeze
+
 class GildedRose
-  def initialize(items)
+  def initialize(items, updaters = DEFAULT_UPDATERS)
     @items = items
+    @updaters = updaters
   end
 
   def update_quality
     @items.each do |item|
-      Updater.for(item).update
+      find_updater_for(item).update(item)
     end
+  end
+
+  private
+
+  def find_updater_for(item)
+    @updaters.find { |updater| updater.handles?(item) }
   end
 end
